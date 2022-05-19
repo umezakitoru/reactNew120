a4ca1fafab56304e900877c336085a5a75829f05
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-05-20 07:34:22 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
 export default function App() {
+  /* map */
+  const list = [1, 2, 3, 4];
+  const dataList = [
+    {
+      name: "taro",
+      age: 32
+    },
+    {
+      name: "jiro",
+      age: 52
+    }
+  ];
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>map-practice</h1>
+      {list.map((number) => (
+        <div>{number * 2}</div>
+      ))}
+      {dataList.map((data) => (
+        <div>
+          {data.name}
+          <span>{data.age}</span>
+        </div>
+      ))}
     </div>
   );
 }
